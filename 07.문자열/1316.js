dc3619a44a98d28e0fc4d97e3e0065be3c60e0d9
shj3497 <shj3497@gmail.com>
2021-05-21 21:10:54 +0900

    2021-05-21 Ch07 Finish

--- 07.문자열/1316.js
+++ 07.문자열/1316.js
@@ -23,29 +23,37 @@ rl.on('line', line => {
     const word_char = word.split('');
     let bool = false;
 
+    // map은 break가 되지 않으므로 my_flag라는 boolean값과 
+    // 조건문을 추가하여 break가 되는 지점을 만들어 주었다.
+    // 'dldyddlwl'님의 조언
+    let my_flag = false;
+
     // 각 단어의 문자의 처음나오는 f_index번호와, 마지막 l_index번호를 구해서
     // f_index와 l_index번호가 다르다면 f_index부터 l_index까지
     // 반복문을 통해 그 사이에 있는 문자가 현재 char인지 확인하여 판단한다.
+
     word_char.map(char => {
-      // console.log(`char: ${char}`);
+      
       const f_index = word.indexOf(char);
       const l_index = word.lastIndexOf(char);
-      // console.log(`f_index: ${f_index}, l_index: ${l_index}`);
-
-      if(f_index !== l_index){
-        for(let j=f_index; j<l_index; j++){
-          if(word.charAt(j) === char){
-            bool = true;
-          }else{
-            bool = false;
-            break;
+      
+      if(my_flag !== true){
+        if(f_index !== l_index){
+          for(let j=f_index; j<=l_index; j++){
+            if(word.charAt(j) === char){
+              bool = true;
+            }else{
+              bool = false;
+              my_flag = true;
+              break;
+            }
           }
+        }else{
+          bool = true;
         }
-      }else{
-        bool = true;
       }
     })
-    // console.log(`bool : ${bool}`)
+
     if(bool){
       count++;
     }
--- 07.문자열/README.md
+++ 07.문자열/README.md
@@ -4,3 +4,27 @@ String.prototype.replaceAll = function (org, dest) {
   return this.split(org).join(dest);
 };
 ```
+
+- map함수는 break가 걸리지 않는다.
+- [1316번](./1316.js)
+
+```javascript
+// 1 abacc 를 실행하면 그룹단어라고 인식한다.
+word_char.map((char) => {
+  const f_index = word.indexOf(char);
+  const l_index = word.lastIndexOf(char);
+
+  if (f_index !== l_index) {
+    for (let j = f_index; j <= l_index; j++) {
+      if (word.charAt(j) === char) {
+        bool = true;
+      } else {
+        bool = false;
+        break; // break 안걸림
+      }
+    }
+  } else {
+    bool = true;
+  }
+});
+```
