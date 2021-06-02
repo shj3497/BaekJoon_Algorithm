3c677d06e403d54f2c0294f223bb7962a61d490f
shj3497 <shj3497@gmail.com>
2021-06-03 00:32:33 +0900

    2021-06-03

--- 08.기본 수학1/10757.js
+++ 08.기본 수학1/10757.js
@@ -13,79 +13,60 @@ rl.on('line', line => {
   input = line.split(' ');
 }).on('close', () => {
 
-  const A = BigInt(input[0]);
-  const B = BigInt(input[1]);
-  const result = A + B;
-  console.log(result.toString());
+  // const A = BigInt(input[0]);
+  // const B = BigInt(input[1]);
+  // const result = A + B;
+  // console.log(result.toString());
   
-  // // 문자열로 입력받은 큰수를 쪼개서 배열에 넣은것을 순서를 바꿔주었다.
-  // // 이유 : 배열의 요소를 뽑아서 더할것이기 때문에 1의자리 숫자가 배열 0번째에 오도록!
-  // const A = input[0].split('').map(item => parseInt(item)).reverse();
-  // // console.log(A);
-  // const B = input[1].split('').map(item => parseInt(item)).reverse();
-  // // console.log(B);
+  // 문자열로 입력받은 큰수를 쪼개서 배열에 넣은것을 순서를 바꿔주었다.
+  // 이유 : 배열의 요소를 뽑아서 더할것이기 때문에 1의자리 숫자가 배열 0번째에 오도록!
+  const A = input[0].split('').map(item => parseInt(item)).reverse();
+  // console.log(A);
+  const B = input[1].split('').map(item => parseInt(item)).reverse();
+  // console.log(B);
 
-  // // 배열의 가장 마지막에 0을 추가한 이유
-  // // ex) 500 + 501 = 1001 로 4자리수가 나오는데 각 배열은 3자리이기 때문에 0을 추가해주었다.
-  // if(A.length === B.length){
-  //   if(A[A.length-1] >= 5 && B[B.length-1] >= 5){
-  //     A.push(0);
-  //     B.push(0);
-  //   }
-  // }
-  // let sum = 0;
-  // let sum_arr = [];
-  // let bool = false;
-  // let result = ''
+  // 자리수 맞춰주기!
+  if(A.length !== B.length){
+    if(A.length > B.length){
+      for(let i=B.length; i<A.length; i++){
+        B.push(0);
+      }
+    }else if(A.length < B.length){
+      for(let i=A.length; i<B.length; i++){
+        A.push(0);
+      }
+    }
+  }
+
+  let sum_arr = [];
+  let result = ''
   
-  // // 첫번째 입력받은 숫자가 두번째 입력받은 숫자보다 큰경우
-  // if(A.length >= B.length){
-  //   for(let i=0; i<A.length; i++){
-      
-  //     if(B[i] === undefined){
-  //       B[i] = 0;
-  //     }
-      
-  //     sum = A[i] + B[i];
-      
-  //     if(bool){
-  //       sum = sum + 1;
-  //     }
+  // 첫번째 입력받은 숫자가 두번째 입력받은 숫자보다 큰경우
+  for(let i=0; i<A.length; i++){
+    let sum = 0;
 
-  //     if(sum >= 10){
-  //       sum_arr.push(sum - 10);
-  //       bool = true;
-  //     }else{
-  //       sum_arr.push(sum);
-  //       bool = false;
-  //     }
-  //   }
-  // }else{
-  //   for(let i=0; i<B.length; i++){
-      
-  //     if(A[i] === undefined){
-  //       A[i] = 0;
-  //     }
-      
-  //     sum = A[i] + B[i];
-      
-  //     if(bool){
-  //       sum = sum + 1;
-  //     }
+    if(B[i] === undefined){
+      B[i] = 0;
+    }
+    
+    if(sum_arr[i] === undefined){
+      sum_arr[i] = 0;
+    }
 
-  //     if(sum >= 10){
-  //       sum_arr.push(sum - 10);
-  //       bool = true;
-  //     }else{
-  //       sum_arr.push(sum);
-  //       bool = false;
-  //     }
-  //   }
-  // }
-  // sum_arr = sum_arr.reverse()
-  // // console.log(sum_arr);
-  // sum_arr.map(item => {
-  //   result = result + `${item}`;
-  // })
-  // console.log(result);
+    sum = A[i] + B[i] + sum_arr[i];
+    
+    if(sum >= 10){
+      sum_arr[i] = sum - 10;
+      sum_arr[i+1] = 1;
+    }else{
+      sum_arr[i] = sum;
+    }
+  }
+  // console.log(sum_arr);  
+  sum_arr = sum_arr.reverse()
+  
+  sum_arr.map(item => {
+    result = result + `${item}`;
+  })
+  console.log(result);
 })
